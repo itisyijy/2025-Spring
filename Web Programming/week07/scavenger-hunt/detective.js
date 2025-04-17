4bbe92ba4be5b3e3b4d7b4d091dad3d476fc0dd9
itisyijy <jy02060577@gmail.com>
2025-04-17 13:56:49 +0900

    second clue done

--- Web Programming/week07/scavenger-hunt/detective.js
+++ Web Programming/week07/scavenger-hunt/detective.js
@@ -1,24 +1,71 @@
 document.addEventListener("DOMContentLoaded", fetchFirstClue);
 
 let baseURL = "http://114.71.51.49:60003/";
+let json_output;
 
 function fetchFirstClue() {
   fetch(baseURL + "api/first-clue")
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
-      id("clue1").innerHTML = json["innerhtml"];
+      json_output = json;
+      const containerClue1 = id("clue1");
+      containerClue1.innerHTML = json["innerhtml"];
+      const clue1Button = id("boton1");
+      clue1Button.addEventListener("click", fetchSecondClue);
     })
     .catch((error) => console.log(error));
 }
 
-function getSecondClue() {}
+function getSecondClue() {
+  let secondClue = "";
+  json_output["clueArray"].forEach((element) => {
+    let extract = element[element.length % 5];
+    secondClue += extract;
+  });
+  return "api/" + secondClue;
+}
 
-async function fetchSecondClue() {}
+async function fetchSecondClue() {
+  let secondClue = await getSecondClue();
+  fetch(baseURL + secondClue)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      json_output = json;
+      const containerClue2 = id("clue2");
+      containerClue2.innerHTML = json["innerhtml"];
+      const clue2Button = id("boton2");
+      clue2Button.addEventListener("click", () => {
+        json["boxes"].forEach((element) => {
+          let box = document.createElement("div");
+          box.textContent = element["text"];
+          box.id = element["id"];
+          id("key-container").appendChild(box);
+        });
+        id("key-container").style.display = "flex";
+        fetchThirdClue();
+      });
+    })
+    .catch((error) => console.log(error));
+}
 
-function getThirddClue() {}
+function getThirddClue() {
+  return "api/" + "crud" + "200" + "rest" + "ssr";
+}
 
-async function fetchThirdClue() {}
+async function fetchThirdClue() {
+  let thirdClue = getThirddClue();
+  fetch(baseURL + thirdClue)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      json_output = json;
+      const containerClue3 = id("clue3");
+      containerClue3.innerHTML = json["innerhtml"];
+    })
+    .catch((error) => console.log(error));
+}
 
 function handleFinalClue() {}
 
