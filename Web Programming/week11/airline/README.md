26e4071813d8e660a3cbeb7e0fbd4b95ce2e7ffa
itisyijy <jy02060577@gmail.com>
2025-05-21 00:03:47 +0900

    WP W11 Prcatice

--- Web Programming/week11/airline/README.md
+++ Web Programming/week11/airline/README.md
@@ -1,7 +1,13 @@
 # initializing env setting
+
 `npm init`
+
 `npm install sqlite sqlite3`
+
 `npm install express`
 
+`npm install nodemon`
+
 # execution
+
 `node database.js`
--- Web Programming/week11/airline/database.js
+++ Web Programming/week11/airline/database.js
@@ -53,12 +53,60 @@ async function executeQuery() {
 // testing code
 // executeQuery();
 
+//Practice yourself: Query all flights longer than 600 minutes
+async function durationFilter(target) {
+  try {
+    let db = await getDBConnection(DB_NAME);
+
+    const sql = "SELECT * FROM flights WHERE duration > ?;";
+    let result = await db.all(sql, [target]);
+    console.log(result);
+    await db.close();
+  } catch (err) {
+    console.error("Failed to load database:", err.message);
+  }
+}
+// testing code
+// durationFilter(600);
+
+//Practice yourself: Find all flights arriving in Paris
+async function destinationFinder(destination) {
+  try {
+    let db = await getDBConnection(DB_NAME);
+
+    const sql = "SELECT * FROM flights WHERE destination = ?;";
+    let result = await db.all(sql, [destination]);
+    console.log(result);
+    await db.close();
+  } catch (err) {
+    console.error("Failed to load database:", err.message);
+  }
+}
+// testing code
+// destinationFinder("Paris");
+
+//Practice yourself: List flights originating from New York, Seoul, or Paris
+async function originFinder(destination) {
+  try {
+    let db = await getDBConnection(DB_NAME);
+
+    const placeholders = destination.map(() => "?").join(", ");
+    const sql = `SELECT * FROM flights WHERE destination IN (${placeholders});`;
+    let result = await db.all(sql, destination);
+    console.log(result);
+    await db.close();
+  } catch (err) {
+    console.error("Failed to load database:", err.message);
+  }
+}
+// testing code
+// originFinder(["New York", "Seoul", "Paris"]);
+
 async function executeInsert() {
   try {
     let db = await getDBConnection(DB_NAME);
 
-    const sql =
-      'INSERT INTO flights (origin, destination, duration) VALUES ("Incheon", "Monterrey", 960);';
+    const sql = `INSERT INTO flights (origin, destination, duration) VALUES ("Incheon", "Monterrey", 960);`;
     let result = await db.exec(sql);
     console.log(result);
     await db.close();
@@ -85,6 +133,25 @@ async function runInsert() {
 // testing code: run() YES RETURN
 // runInsert();
 
+// Practice yourself: Insert three new flights into the flights table
+async function runInsert(origin, destination, duration) {
+  try {
+    let db = await getDBConnection(DB_NAME);
+
+    const sql = `INSERT INTO flights (origin, destination, duration) VALUES (?, ?, ?);`;
+    let result = await db.run(sql, [origin, destination, duration]);
+    console.log(result);
+    await db.close();
+  } catch (err) {
+    console.error("Failed to load database:", err.message);
+  }
+}
+// Practice yourself: Insert three new flights into the flights table
+// // From Incheon to San Francisco, Incheon to Amsterdam, Paris to Rome
+// runInsert("Incheon", "San Francisco", 635);
+// runInsert("Incheon", "Amsterdam", 835);
+// runInsert("Paris", "Rome", 125);
+
 async function extractData() {
   try {
     let db = await getDBConnection(DB_NAME);
@@ -103,3 +170,25 @@ async function extractData() {
 }
 // testing code
 // extractData();
+
+// Practice yourself: Create an API endpoint /flights that returns a JSON listing all the flights
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.get("/flights", async function (req, res) {
+  try {
+    let db = await getDBConnection(DB_NAME);
+    let result = await db.all("SELECT * FROM flights");
+    await db.close();
+
+    console.log("Endpoint /flights received a GET request.");
+    res.json(result);
+  } catch (err) {
+    console.error("Failed to load database:", err.message);
+  }
+});
+
+app.listen(PORT, function () {
+  console.log(`Our first node app listening on port ${PORT}`);
+});
