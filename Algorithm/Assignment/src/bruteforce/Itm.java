fb9294905ab8c2cc6020f4b46d8b389e59774e28
itisyijy <jy02060577@gmail.com>
2025-05-28 22:00:43 +0900

    algorithm bruteforce

--- Algorithm/Assignment/src/bruteforce/Itm.java
+++ Algorithm/Assignment/src/bruteforce/Itm.java
@@ -3,6 +3,14 @@ package bruteforce;
 public class Itm {
 
   public int maxSubarraySum(int[] arr) {
-    return 0;
+    int max = Integer.MIN_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+      int tmp = 0;
+      for (int j = i; j < arr.length; j++) {
+        tmp += arr[j];
+        max = Math.max(max, tmp);
+      }
+    }
+    return max;
   }
 }
