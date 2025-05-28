92539966721fc48d10120ec79c2522cba3f0515f
itisyijy <jy02060577@gmail.com>
2025-05-28 16:04:32 +0900

    TST

--- Algorithm/Assignment/src/shortest/TestCases.java
+++
@@ -1,8 +0,0 @@
-package shortest;
-
-public class TestCases {
-    public static void main(String[] args) {
-        Itm itm = new Itm();
-        System.out.println("2 : "+itm.minKnightMoves(9, 9, 3, 5, 2, 8));
-    }
-}
