072492e72adbac7adff5476053f29a40d258ff6c
itisyijy <jy02060577@gmail.com>
2025-06-03 16:38:41 +0900

    dnC comment

--- Algorithm/Assignment/src/dnC/Main.java
+++ Algorithm/Assignment/src/dnC/Main.java
@@ -6,6 +6,6 @@ public class Main {
     Itm itm = new Itm();
     System.out.println("24: " + (itm.processDigits("1234") == 24 ? 24 : -1));   // Output: 24
     System.out.println("43: " + (itm.processDigits("12345") == 43 ? 43 : -1));  // Output: 43
-    System.out.println("2: " + (itm.processDigits("12") == 2 ? 2 : -1));  // Output: 43
+    System.out.println("2: " + (itm.processDigits("12") == 2 ? 2 : -1));  // Output: 2
   }
 }
